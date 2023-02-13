baba017adeec266bf4c22d7a1612c834fdd9a0cc
Roger Consul <rogerconsul@yahoo.com.br>
2023-02-13 20:30:05 -0300

    Meu primeiro CRUD com Rails

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  root "articles#index"
+
+  resources :articles do
+    resources :comments
+  end
+
+  # get "/articles", to: "articles#index"
+  # get "/articles/:id", to: "articles#show"
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
